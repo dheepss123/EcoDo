cbb359186c6d143a4d14aed432d039f9f498eba2
fauzanrizkyanag <fauzanrizkyana@gmail.com>
2023-12-08 01:26:33 +0700

    Fix bug close force

--- md/EcoDo/app/src/main/java/com/bangkit/ecodo/MainActivity.kt
+++ md/EcoDo/app/src/main/java/com/bangkit/ecodo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main2)
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
--- md/EcoDo/settings.gradle.kts
+++ md/EcoDo/settings.gradle.kts
@@ -15,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "EcoDo"
 include(":app")
- 

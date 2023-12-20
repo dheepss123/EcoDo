3fa823b0f7bfaf5165c6828809c0a644ba7bd97e
fauzanrizkyanag <fauzanrizkyana@gmail.com>
2023-12-20 19:07:53 +0700

    fix bug

--- md/EcoDo/app/src/main/java/com/bangkit/ecodo/ui/recommendation/video/VideoRecomendationViewModel.kt
+++ md/EcoDo/app/src/main/java/com/bangkit/ecodo/ui/recommendation/video/VideoRecomendationViewModel.kt
@@ -1,14 +1,8 @@
 package com.bangkit.ecodo.ui.recommendation.video
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.bangkit.ecodo.data.model.VideoModel
 import com.bangkit.ecodo.data.repository.VideoRepository
-import com.bangkit.ecodo.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,19 +10,5 @@ class VideoRecomendationViewModel @Inject constructor(
     private val videorepository: VideoRepository,
 ) : ViewModel() {
 
-//    private val _videoList = MutableLiveData<Resource<List<VideoModel>>>()
-//    val videoList: LiveData<Resource<List<VideoModel>>> get() = _videoList
-//
-//    fun getVideos() {
-//        viewModelScope.launch {
-//            _videoList.value = Resource.Loading
-//            try {
-//                val response = videorepository.getVideos()
-//                _videoList.value = response.value
-//            } catch (e: Exception) {
-//                _videoList.value = Resource.Error(e.localizedMessage?.toString() ?: "Unknown Error")
-//            }
-//        }
-//    }
     fun getVideos() = videorepository.getVideos()
 }
